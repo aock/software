8c627cb7de9482faf7c16c9d4d7afbb37c67106e
Alexander Mock <amock@uos.de>
2019-11-04 10:51:30 +0100

    added cmake test

---
+++ opencv/cmake_test/example.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main(int argc, char** argv )
+{
+    std::cout << CV_VERSION << std::endl;
+
+    return 0;
+}
