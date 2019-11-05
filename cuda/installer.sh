c355b058d33952f3b93289a033cbc04a0f683c5c
Alexander Mock <amock@uos.de>
2019-11-05 10:05:38 +0100

    added cudarc

--- cuda/installer.sh
+++ cuda/installer.sh
@@ -6,11 +6,19 @@ set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
-if [ "$VERSION" == "10.1" ]; then
+if [[ "$VERSION" == "10.1" ]]; then
     DOWNLOADLINK="http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run"
-elif [ "$VERSION" == "10.0" ]; then
+elif [[ "$VERSION" == "10.0" ]]; then
     DOWNLOADLINK="https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux"
+elif [[ "$VERSION" == "9.2" ]]; then
+    DOWNLOADLINK="https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux"
+elif [[ "$VERSION" == "9.1" ]]; then
+    DOWNLOADLINK="https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux"
+elif [[ "$VERSION" == "9.0" ]]; then
+    DOWNLOADLINK="https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run"
 fi
+
+echo "Download CUDA installer from: $DOWNLOADLINK"
 
 wget -q --show-progress -O cuda.run "$DOWNLOADLINK"
 chmod +x cuda.run
@@ -44,3 +52,5 @@ else
 fi
 
 rm cuda.run
+
+echo "Finished! Please either add the commands of .cudarc to .bashrc of your home folder or source the .cudarc from your .bashrc"
