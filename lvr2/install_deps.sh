9902b0de7ddb7251db9a80e41b808439685927d7
Alexander Mock <amock@uos.de>
2019-11-05 12:02:16 +0100

    added lvr2 install deps

---
+++ lvr2/install_deps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo apt-get install libgdal-dev libflann-dev liblz4-dev 
+sudo apt-get install libgsl-dev libvtk7-dev libcgal-dev 
+sudo apt-get install mesa-utils freeglut3-dev
+sudo apt-get install libyaml-cpp-dev
+sudo apt-get install libboost-dev libboost-all-dev
+sudo apt-get install qt5-default
+sudo apt-get install doxygen
